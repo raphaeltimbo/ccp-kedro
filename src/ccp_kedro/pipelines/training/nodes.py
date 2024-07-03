cd38fdba8eeecc457b490380cb94da952a27d11b
raphaeltimbo <raphaelts@petrobras.com.br>
2024-07-03 16:28:26 -0300

    Add calculate flow to pipeline

--- src/ccp_kedro/pipelines/training/nodes.py
+++ src/ccp_kedro/pipelines/training/nodes.py
@@ -19,3 +19,37 @@ def filter_data(
 ):
     return ccp.data_io.filter_data(data, data_type=data_type)
 
+
+def calculate_flow(data_filtered, parameters):
+    # from parameters
+    data_units = parameters["data_units"]
+    operation_fluid = parameters["operation_fluid"]
+    # create density column
+    df = data_filtered
+    df["v_s"] = 0
+    df["speed_sound"] = 0
+    for i, row in df.iterrows():
+        # create state
+        state = ccp.State(
+            p=Q_(row.ps, data_units["ps"]),
+            T=Q_(row.Ts, data_units["Ts"]),
+            fluid=operation_fluid,
+        )
+        df.loc[i, "v_s"] = state.v().m
+        df.loc[i, "speed_sound"] = state.speed_sound().m
+
+    # check if flow_v or flow_m is in the DataFrame
+    if "flow_v" in df.columns:
+        # create flow_m column
+        df["flow_m"] = (
+            Q_(df["flow_v"].array, data_units["flow_v"]) * Q_(df["v_s"].array, "m³/kg")
+        ).m
+    elif "flow_m" in df.columns:
+        # create flow_v column
+        df["flow_v"] = (
+            Q_(df["flow_m"].array, data_units["flow_m"]) / Q_(df["v_s"].array, "m³/kg")
+        ).m
+    else:
+        raise ValueError("Flow rate not found in the DataFrame.")
+
+    return df
--- src/ccp_kedro/pipelines/training/pipeline.py
+++ src/ccp_kedro/pipelines/training/pipeline.py
@@ -1,6 +1,6 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import filter_data
+from .nodes import filter_data,calculate_flow
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -12,5 +12,11 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="data_filtered",
                 name="filter_data_node",
             ),
+            node(
+                func=calculate_flow,
+                inputs=["data_filtered", "parameters"],
+                outputs="data_with_flow",
+                name="calculate_flow_node",
+            )
         ]
     )
