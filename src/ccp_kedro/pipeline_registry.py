0626034a3905d56e2aa744fb14b26c40f77d6b02
raphaeltimbo <raphaelts@gmail.com>
2024-07-25 17:58:28 -0300

    Fix registry

--- src/ccp_kedro/pipeline_registry.py
+++ src/ccp_kedro/pipeline_registry.py
@@ -4,7 +4,7 @@ from typing import Dict
 from kedro.pipeline import Pipeline, pipeline
 
 from ccp_kedro.pipelines import training as training
-# from ccp_kedro.pipelines import online as online
+from ccp_kedro.pipelines import online as online
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -14,9 +14,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
     training_pipeline = training.create_pipeline()
-    # online_pipeline = online.create_pipeline()
+    online_pipeline = online.create_pipeline()
     return {
         "training": training_pipeline,
-        # "online": online_pipeline,
-        "__default__": training_pipeline,# + online_pipeline,
+        "online": online_pipeline,
+        "__default__": training_pipeline + online_pipeline,
     }
